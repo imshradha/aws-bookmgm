2d87c5904175b2647161c95eb686071e0b85874d
Swetarun <er.shweta.singh990@gmail.com>
2022-05-10 00:35:57 +0530

    Validation loop

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -23,24 +23,26 @@ const createUser= async function (req, res) {
         if(!isValidRequestBody(body)){
             return res.status(400).send({status:false,message:"Invalid request parameters please provide author details"})
         }
-        const {title,name,phone,email,password,address}=req.body
-        if(!isValid(title)){
-            return res.status(400).send({status:false,message:"Please enter title"})
+  //Added loop for validation
+        let required= ["title","name","phone", "email", "password"]
+        let keys= Object.keys(body)
+       
+        for(let i=0; i<required.length; i++){
+            if(keys.includes(required[i])) continue
+             else return res.status(400).send({ status: false, msg: `Required field - ${required[i]}`})
         }
+       //checking for empty values
+        for (const property in body) {
+            if(typeof body[property] == 'string' && body[property].trim().length == 0)
+            return res.status(400).send({ status: false, msg: `Required field - ${property}`})
+          else continue
+        }
+        const {title,name,phone,email,password,address}=req.body
         
         if(!isValidTitle(title)){
             return res.status(400).send({status:false,message:"Please provide right title"})
-
         }
-        if(!isValid(name)){
-            return res.status(400).send({status:false,message:"Please enter name"})
-        }
-        if (!isValid(phone)) {
-            res
-              .status(400)
-              .send({ status: false, message: " Please enter phone no" });
-            return;
-          }
+       
         const isPhone=await userModel.findOne({phone:phone})
         if(isPhone){
               return res.status(400).send({status:false,message:"phone no is already registered"})
@@ -48,15 +50,9 @@ const createUser= async function (req, res) {
       
         const validMobile = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(phone);
         if (!validMobile) {
-            return res
-              .status(400)
-              .send({ status: false, msg: "Enter valid mobile no." });
+            return res.status(400).send({ status: false, msg: "Enter valid mobile no." });
         }
     
-        if(!isValid(email)){
-            return res.status(400).send({status:false,message:"Please enter email"})
-        }
-        
         if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))){
            return res.status(400).send({status:false,message:"Email should be valid"})
         }
@@ -65,61 +61,48 @@ const createUser= async function (req, res) {
             return res.status(400).send({status:false,message:"email address is already registered"})
         }
 
-        if(!isValid(password)){
-            return res.status(400).send({status:false,message:"Please enter password"})
-        }
         const schema = new passwordValidator();
         schema.is().min(8).max(15)
         if (!schema.validate(password)) {
             return res.status(400).send({ status: false, msg: "length of password should be 8-15 characters" })
         }
-        if(typeof address.pincode==NaN){
+        if(isNaN(address.pincode)){ //typeof NaN--not working
             res.status(400).send({status:false,message:"Only number is  allowed"})
         }
         if (Object.values(address.pincode).length < 6 || (address.pincode).length > 6) {
             return res.status(400).send({ status: false, msg: "Pincode should be of 6 digits" })
         }
          
-        if (!/^[a-zA-Z]+$/.test(address.city)) {
+        if (!/^[a-zA-Z ]+$/.test(address.city)) {
             return res.status(400).send({ status: false, message: "City name can only be alphabetically" });
         }
         const user=await userModel.create(body)
         res.status(201).send({status:true,message:"created successfully",data:user})
     }
     catch (err) {
-        
         res.status(500).send({ status:false, data: err.message })
     }
-
 }
 
 const loginUser = async function (req, res) {
     try {
       const requestBody = req.body;
       if (!isValidRequestBody(requestBody)) {
-        return res
-          .status(400)
-          .send({ status: false, msg: "please provide data to signIn" });
+        return res.status(400).send({ status: false, msg: "please provide data to signIn" });
       }
       const { email, password } = requestBody;
   
       if (!isValid(email)) {
-        return res
-          .status(400)
-          .send({ status: false, msg: "please provide email" });
+        return res.status(400).send({ status: false, msg: "please provide email" });
       }
       if (!isValid(password)) {
-        return res
-          .status(400)
-          .send({ status: false, msg: "please provide password" });
+        return res.status(400).send({ status: false, msg: "please provide password" });
       }
       // ADD PASSWORD VALIDATOR
   
       const findEmailAndPassword = await userModel.findOne({email: email,password:password });
       if (!findEmailAndPassword) {
-        return res
-          .status(400)
-          .send({ status: false, msg: "Please provide valid credentials" });
+        return res.status(401).send({ status: false, msg: "Please provide valid credentials" }); //401- for unauthorized--it lacks valid authentication credentials
       }
   
       const userId=findEmailAndPassword._id
@@ -132,9 +115,7 @@ const loginUser = async function (req, res) {
             iat: Math.floor(Date.now() / 1000),
             exp: Math.floor(Date.now() / 1000) + 10*60*60
           }, 'bookManagement-project3')
-        res
-          .status(200)
-          .send({ msg: "user login sucessfully", token:token });
+        res.status(200).send({ msg: "user login sucessfully", token:token });
       }
     } catch (err) {
       console.log(err);
